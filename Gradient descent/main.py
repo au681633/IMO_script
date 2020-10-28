784a8aabadeb4106c6d0f8aff3d0529cda1457bd
au681633 <202005664@post.au.dk>
2020-10-28 09:38:49 +0100

    first push

---
+++ Gradient descent/main.py
@@ -0,0 +1,35 @@
+from sympy import *
+import numpy as np
+
+def gradient():
+    # first x
+    xres = 1
+
+    x = Symbol('x')
+
+    # function
+    y = (x-1)**4 + sin(x)**2
+
+    yprime = y.diff(x)
+    fprime = lambdify(x, yprime, 'numpy')
+    f = lambdify(x, y, 'numpy')
+
+    print(y)
+    print(yprime)
+
+    print("")
+
+    k = 1
+
+    # only works if the right lambda is chosen
+
+    while abs(fprime(xres)) >= 0.5:
+        xres = xres - (2**(-k) * fprime(xres))
+        k += 1
+
+    yres = f(xres)
+
+    return("Minimum is at (" + str(xres) + "," + str(yres) + ")")
+
+print(gradient())
+
