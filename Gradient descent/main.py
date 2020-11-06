e1a95d792acb78067cd83f2fabd22ae166f03cde
au681633 <202005664@post.au.dk>
2020-11-06 14:59:51 +0100

    second push

--- Gradient descent/main.py
+++ Gradient descent/main.py
@@ -4,32 +4,22 @@ import numpy as np
 def gradient():
     # first x
     xres = 1
-
     x = Symbol('x')
-
     # function
     y = (x-1)**4 + sin(x)**2
-
     yprime = y.diff(x)
+    # creates function in python
     fprime = lambdify(x, yprime, 'numpy')
     f = lambdify(x, y, 'numpy')
- # ok
     print(y)
     print(yprime)
-
     print("")
 
-    k = 1
-
     # only works if the right lambda is chosen
+    while abs(fprime(xres)) >= 0.0001:
+        xres = xres - (1/4 * fprime(xres))
 
-    while abs(fprime(xres)) >= 0.5:
-        xres = xres - (2**(-k) * fprime(xres))
-        k += 1
-
-    yres = f(xres)
-
-    return("Minimum is at (" + str(xres) + "," + str(yres) + ")")
+    return("Minimum is at (" + str(xres) + "," + str(f(xres)) + ")")
 
 print(gradient())
 
